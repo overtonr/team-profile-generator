2dd87ee99c042d161a868ad1d4d48ed893d1e4d0
Rebecca Overton <rebeccaoverton@Rebeccas-Air.attlocal.net>
2022-10-25 14:53:59 -0700

    outline comments for index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-console.log("js file linked!")
 //need to link inquirer prompts from all employees : so that when node is ran through this file, user can answer the prompts
 
+function managerInquire();
+
+function engineerInquire();
+
+function internInquire();
+
+function completeTeam(); //called when user selects option to complete team profile
+
+//need an empty array where constructed objects can be pushed
+//array later referenced when generating HTML
+//different array for each type of employee?
+
+function init();//should start with manager inquirer prompts
+init();
