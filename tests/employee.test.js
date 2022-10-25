b15e56e465f70c14845dbec0cc4cb2c08b1a05f6
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-10-25 11:27:53 -0700

    employee.test.js added

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,4 +1,18 @@
+const employeeJS = require('../lib/employee');
 //can an object be created from the class
 //every property assigned to class: return;
 //every method in the class
 //mock data returns: test passed
+
+describe('employeeJS',() => {
+    it('Should return the object of the employee profile', () => {
+        const newEmployee = new Employee("Rebecca",01,"rebecca@gmail.com");
+        expect(newEmployee.getName()).toEqual("Rebecca");
+        expect(newEmployee.getId()).toEqual(01);
+        expect(newEmployee.getEmail()).toEqual("rebecca@gmail.com");
+        expect(newEmployee.getRole()).toEqual("Employee");
+        
+    });
+    //it('Should throw an error if no input is provided', () => {
+    //is error for empty string required?
+});
